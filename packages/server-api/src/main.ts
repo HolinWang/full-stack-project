726de9a245094ecc2ccb6ad25fa7c17c43c76d67
Holin <Holin.wang@outlook.com>
2023-11-27 21:46:14 +0800

    利用Pipe通道实现数据校验

--- packages/server-api/src/main.ts
+++ packages/server-api/src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { generateDocument } from './doc';
-
+import {ValidationPipe} from "@nestjs/common";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  // 添加数据校验 pipe全局管道
+  app.useGlobalPipes(new ValidationPipe());
 
   // create swagger document
   generateDocument(app);
--- packages/server-api/src/user/dto/create-user.dto.ts
+++ packages/server-api/src/user/dto/create-user.dto.ts
@@ -1,31 +1,21 @@
 import { ApiProperty } from "@nestjs/swagger";
-
+import { IsNotEmpty, Matches, Max, Min, Length, IsEmail, isEmail } from 'class-validator';
 export class CreateUserDto {
-  @ApiProperty(
-    {
-      example:"Holin"
-    }
-  )
-  userName:string;
+  @ApiProperty({example: "Holin"})
+  @IsNotEmpty()
+  userName: string;
 
-  @ApiProperty(
-    {
-      example:18
-    }
-  )
-  age:number;
+  @ApiProperty({example: 18})
+  age: number;
 
-  @ApiProperty(
-    {
-      example:"176****8057"
-    }
-  )
+
+
+  @ApiProperty({example: "176****8057"})
+  @Matches(/^1\d{10}$/g, { message: '请输入手机号' })
   phoneNumber: string;
 
-  @ApiProperty(
-    {
-      example:"Holin@outlook.com"
-    }
-  )
-  email:string;
+  @ApiProperty({example: "Holin@outlook.com"})
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
 }
