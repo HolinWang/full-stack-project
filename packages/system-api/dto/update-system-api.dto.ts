d3c7396064d9404dadc6b71f6a407fc6432445d1
Holin <Holin.wang@outlook.com>
2024-11-10 14:32:02 +0800

    remove system api

--- packages/system-api/dto/update-system-api.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateSystemApiDto } from './create-system-api.dto';
-
-export class UpdateSystemApiDto extends PartialType(CreateSystemApiDto) {}
--- packages/system-api/system-api.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SystemApiService } from './system-api.service';
-import { SystemApiController } from './system-api.controller';
-
-@Module({
-  controllers: [SystemApiController],
-  providers: [SystemApiService],
-})
-export class SystemApiModule {}
